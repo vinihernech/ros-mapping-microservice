41dd4a6053af978c63ad379e36399dd80da341e1
labsea6 <viniciushernech07@gmail.com>
2022-09-06 17:50:08 -0300

    chore: change rpc

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 import actionlib
 import rospy
-import argparse
 import yaml
-import sys
 import numpy as np
-from nav_msgs.msg import OccupancyGrid, MapMetaData
 from math import cos, sin, pi
 from is_msgs.camera_pb2 import FrameTransformation
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseFeedback, MoveBaseResult
@@ -12,13 +9,11 @@ from tf.transformations import quaternion_from_euler
 from is_wire.core import Channel, Message, Subscription, StatusCode, Status, Logger
 from is_msgs.robot_pb2 import RobotTaskRequest
 from is_wire.rpc import ServiceProvider, LogInterceptor
-from google.protobuf.empty_pb2 import Empty
-from maprequest_pb2 import MapRequest, MapRequestReply
+from maprequest_pb2 import MapRequest
 from streamChannel import StreamChannel
 from std_srvs.srv import Trigger, TriggerRequest
 from utils import rotationZ_matrix, isframe_to_robotframe
 import roslaunch
-
 
 def reset_map():
    rospy.wait_for_service('/reset_map')
@@ -26,10 +21,13 @@ def reset_map():
    sos = TriggerRequest()
    result = resetMap(sos)
 
-def save_map():
+def save_map(map_id):
+   cli_args = ['save_map.launch', f'map_id:={map_id}']
+   roslaunch_args = cli_args[1:]
+   roslaunch_file = [(roslaunch.rlutil.resolve_launch_arguments(cli_args)[0], roslaunch_args)]
    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
    roslaunch.configure_logging(uuid)
-   launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/labsea6/Documents/ros-mapping-microservice/etc/save_map.launch/test.launch"])
+   launch = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
    launch.start()
    launch.spin()
 
@@ -38,7 +36,6 @@ def get_robot_pose(config):
     subscription = Subscription(channel_recontruction)
     aruco_id = config['aruco_id']
     subscription.subscribe(topic=f"localization.{aruco_id}.aruco")
-
     message = channel_recontruction.consume_last()
     if type(message) != bool:
         f = message.unpack(FrameTransformation)
@@ -50,28 +47,30 @@ def get_robot_pose(config):
     else:
         return message
 
-
-def get_is_points(message,ctx):
-
+def get_is_points(message):
     poses = list(message.poses)
-    #initialPose = get_robot_pose(config)
-    initialPose = [0,0,0]
+    if config['is_reconstruction']:
+        initialPose = get_robot_pose(config)
+    else:
+        initialPose = config['initial_pose']
     log.info(f"new map request ID: {message.id}")
-    log.info("start mapping ...")
+    log.info("reset current map ...")
     reset_map()
+    log.info("starting a new mapping ...")
     for i in range(len(poses)):
         x = poses[i].position.x
         y = poses[i].position.y
         theta = poses[i].orientation.yaw
         pose = [x,y,theta]
         pose = isframe_to_robotframe([pose],initialPose)
-        send_goal(pose[0])
-        save_map()
-    #map_reply = listener()
-    log.info("map completed successfully") 
+        send_goal(pose[0],client)
+        save_map(f'{message.id}_{i}')
+    save_map(f'{message.id}_final')
+    log.info("map completed successfully")
+    client.cancel_all_goals()
     return Status(StatusCode.OK)
 
-def send_goal(pose):
+def send_goal(pose, client):
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = 'map' 
     goal.target_pose.pose.position.x = pose[0]
@@ -81,55 +80,42 @@ def send_goal(pose):
     goal.target_pose.pose.orientation.z = quaternion[2]
     goal.target_pose.pose.orientation.w = quaternion[3]
     client.send_goal(goal)
-    print('setting final position task to  x:{:.2f}, y:{:.2f}, theta: {:.2f}'.format(pose[0],pose[1],pose[2]))
+    log.info('setting final position task to  x:{:.2f}, y:{:.2f}, theta: {:.2f}'.format(pose[0],pose[1],pose[2]))
     client.wait_for_result()
     
+    
 
-# def listener():
-#     #rospy.Subscriber("/map_metadata", MapMetaData , callback)
-#     #rospy.Subscriber("/map", OccupancyGrid , callback1)
-#     map_data = rospy.wait_for_message("/map", OccupancyGrid, timeout = 5)
-#     meta_data = rospy.wait_for_message("/map_metadata", MapMetaData, timeout = 5)
-#     maprequestreply = MapRequestReply()
-#     maprequestreply.width = meta_data.width
-#     maprequestreply.height = meta_data.width
-#     #print(meta_data.origin.position)
-#     maprequestreply.map.extend(map_data.data)
-#     return maprequestreply
 
 if __name__ == '__main__':
-
+    log = Logger(name='Map') 
     try:
-        with open(r'../etc/config/config.yaml') as file:
+        with open(r'config.yaml') as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
     except:
-            print('Unable to load config file')
-    
-    log = Logger(name='Map')
+            log.info('unable to load config file')
     try:
         channel = Channel(config['broker_uri'])  
         log.info("connected to broker")
     except:
-        log.info("Can't connect to broker")
+        log.info("can't connect to broker")
 
     robot_id = config['robot_id']
     topic = "IsRosMapping.{}.MapRequest".format(robot_id)
     subscription = Subscription(channel)   
-    rospy.init_node('send_client_goal')
-    client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
-    rospy.loginfo("Waiting for move base server")
-    client.wait_for_server()  
-
+    subscription.subscribe(topic)
+    is_reconstruction = config['is_reconstruction']
     provider = ServiceProvider(channel)
     logging = LogInterceptor()
     provider.add_interceptor(logging)
-    
-    provider.delegate(
-        topic = topic,
-        function = get_is_points,
-        request_type = MapRequest,
-        reply_type = Empty)
 
-    provider.run()
-  
+    rospy.init_node('send_client_goal')
+    client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+    rospy.loginfo("Waiting for move base server")
+    client.wait_for_server() 
+
+    while True:
+        msg = channel.consume()
+        msg_unpack = msg.unpack(MapRequest)
+        get_is_points(msg_unpack)
+
 
