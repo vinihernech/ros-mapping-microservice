2518e666e4a32c94c01ba31cc04b3e3d24f7ba4f
vinihernech <viniciushernech07@gmail.com>
2022-09-08 12:51:17 -0300

    chore: relocate files

--- src/ros_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#ROS_IP=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
-ROS_IP=10.10.1.140
-ROS_MASTER_URI="http://10.10.3.188:30015"
-export ROS_IP=$ROS_IP
-export ROS_MASTER_URI=$ROS_MASTER_URI
-source /home/labsea6/catkin_ws/devel/setup.bash
